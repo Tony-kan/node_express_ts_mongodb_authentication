4e69727d33a27b9f17365e58c96c87b073c410fe
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-04-06 18:10:41 +0200

    created login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -32,3 +32,47 @@ export const register = async (req: express.Request, res: express.Response) => {
     return res.sendStatus(400);
   }
 };
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    const userByEmail = await getUserByEmail(email).select(
+      "authentication.salt + authentication.password"
+    );
+    const expectedHash = authentication(
+      userByEmail.authentication.salt,
+      password
+    );
+
+    if (!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    if (!userByEmail) {
+      return res.sendStatus(400);
+    }
+
+    if (userByEmail.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+
+    userByEmail.authentication.sessionToken = authentication(
+      salt,
+      userByEmail._id.toString()
+    );
+
+    await userByEmail.save();
+
+    res.cookie("TKAY-AUTH", userByEmail.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(userByEmail).end();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
