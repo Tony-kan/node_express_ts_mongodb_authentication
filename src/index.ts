78ac67194484aaeb04077939c2a07510399eda69
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-04-06 13:50:40 +0200

    setup mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const PORT = 8080;
 const app = express();
@@ -18,3 +19,10 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
+
+const MONGO_URL =
+  "mongodb+srv://tkay:tkay@cluster0.b60e7yy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
