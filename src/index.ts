984806af96a8d613b5b64ea69ad7ce68e9f41878
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-04-06 15:44:17 +0200

    configure environmental variables

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,14 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const PORT = 8080;
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const MONGO_URL = process.env.MONGO_URL;
 
 app.use(cors({ credentials: true }));
 app.use(compression());
@@ -19,9 +24,6 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
-
-const MONGO_URL =
-  "mongodb+srv://tkay:tkay@cluster0.b60e7yy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
